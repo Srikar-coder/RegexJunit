2719740224581e37690add6c43e7b27cc3109721
Srikar <srikarsamanchi1999@gmail.com>
2021-06-27 14:15:52 +0530

    User Validation Uc13

--- UserValidation.java
+++ UserValidation.java
@@ -1,71 +1,67 @@
-package com.bridgelabz.regex;
+package com.bridgelabz.lambdaexpression;
 
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class UserValidator {
-   //First name starts with Cap and has minimum 3 characters
-    public String isValidFirstName(String fname) throws RegistrationException {
-        String regex = "^[A-Z][a-z]{3,15}$";
-        try {
-            if (fname.matches(regex) == true)
-                return "HAPPY";
-            else
-                return "SAD";
-        }catch (NullPointerException e){
-            throw new RegistrationException("Please Enter valid First Name");
-        }
-    }
-    // Last name starts with Cap and has minimum 3 characters
-    public String isValidLastName(String lname) throws RegistrationException {
-        String regex = "^[A-Z][a-z]{3,15}$";
-        try {
-            if (lname.matches(regex) == true)
-                return "HAPPY";
-            else
-                return "SAD";
-        }catch (NullPointerException e){
-            throw new RegistrationException("Please Enter valid Last Name");
-        }
-    }
-    //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
-    public String isValidEmailId(String emailId) throws RegistrationException {
-        String regex = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$";
-        try {
-            if (emailId.matches(regex) == true)
-                return "HAPPY";
-            else
-                return "SAD";
-        }catch(NullPointerException e){
-            throw  new RegistrationException("Please Enter Valid Email Id");
-        }
 
-    }
-    //Country code follow by space and 10 digit number
-    public String isValidMobileNo(String mobileNo) throws RegistrationException {
-        String regex = "(0|91)?\\s([7-9][0-9]{9})";
-        try {
-            if (mobileNo.matches(regex) == true)
-                return "HAPPY";
-            else
-                return "SAD";
-        }catch(NullPointerException e){
-            throw  new RegistrationException("Please Enter Valid Mobile Number");
-        }
-    }
-    /* Password should contain minimum 8 Characters
-     * Password should have at least 1 Upper Case
-     * Password should have at least 1 numeric number
-     * Should have exactly 1 Special Character
-     */
-    public String isValidPassword(String password) throws RegistrationException {
-        String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,12}$";
-        try {
-            if (password.matches(regex) == true)
-                return "HAPPY";
-            else
-                return "SAD";
-        }catch(NullPointerException e){
-            throw new RegistrationException("Please Enter Valid Password");
-        }
+    public static void main(String[] args) {
+
+        //First name starts with Cap and has minimum 3 characters
+        ArrayList<String> firstName = new ArrayList<String>();
+        firstName.add("SriRam");
+        firstName.add("priyanshu");
+        firstName.add("Ganesh");
+        Pattern validFirstName = Pattern.compile("^[A-Z][a-z]{3,15}$");
+        List<String> isValidFirstName = firstName.stream().filter(p -> validFirstName.matcher(p).find()).collect(Collectors.<String>toList());
+        System.out.println("Valid First Name :-");
+        isValidFirstName.forEach(System.out::println);
+
+        // Last name starts with Cap and has minimum 3 characters
+        ArrayList<String> lastName = new ArrayList<String>();
+        lastName.add("sri");
+        lastName.add("sam");
+        lastName.add("rakul");
+        Pattern validLastName = Pattern.compile("^[A-Z][a-z]{3,15}$");
+        List<String> isValidLastName = lastName.stream().filter(p -> validLastName.matcher(p).find()).collect(Collectors.<String>toList());
+        System.out.println("Valid Last Name :-");
+        isValidLastName.forEach(System.out::println);
+
+        //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+        ArrayList<String> emailId = new ArrayList<String>();
+        emailId.add("sri@gmail.com");
+        emailId.add("@priya#.123@.com");
+        emailId.add("mohith.@gmail..co");
+        emailId.add("krunal@gmail.co");
+        Pattern validEmailId = Pattern.compile("^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*" + "@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$");
+        List<String> isValidEmailId = emailId.stream().filter(p -> validEmailId.matcher(p).find()).collect(Collectors.<String>toList());
+        System.out.println("Valid Email Ids :-");
+        isValidEmailId.forEach(System.out::println);
+
+        //Country code follow by space and 10 digit number
+        ArrayList<String> mobileNo = new ArrayList<String>();
+        mobileNo.add("91 9993344553");
+        mobileNo.add("919993344553");
+        mobileNo.add("436789002");
+        mobileNo.add("0 8949393423");
+        Pattern validMobileNo = Pattern.compile("(0|91)?\\s([7-9][0-9]{9})");
+        List<String> isValidMobileNo = mobileNo.stream().filter(p -> validMobileNo.matcher(p).find()).collect(Collectors.<String>toList());
+        System.out.println("Valid Mobile Numbers :-");
+        isValidMobileNo.forEach(System.out::println);
+
+        /* Password should contain minimum 8 Characters
+         * Password should have at least 1 Upper Case
+         * Password should have at least 1 numeric number
+         * Should have exactly 1 Special Character
+         */
+        ArrayList<String> password = new ArrayList<String>();
+        password.add("abc@12");
+        password.add("xyz678$rt");
+        password.add("abcd#%ABC@");
+        password.add("abcABC123");
+        password.add("abc@R1234");
+        Pattern validPassword = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,12}$");
+        List<String> isValidPassword = password.stream().filter(p -> validPassword.matcher(p).find()).collect(Collectors.<String>toList());
+        System.out.println("Valid Password :-");
+        isValidPassword.forEach(System.out::println);
     }
 }
