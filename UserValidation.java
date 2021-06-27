83c018fce8ae7289f543449dfbca6c16a9390de3
Srikar <srikarsamanchi1999@gmail.com>
2021-06-27 09:12:45 +0530

    User Registration Uc2

--- UserValidation.java
+++ UserValidation.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
-
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
     //First name starts with Cap and has minimum 3 characters
     public boolean isValidFirstName(String fname) {
         /**
@@ -15,4 +15,15 @@ public class UserValidation {
         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fname). matches();
     }
+    // Last name starts with Cap and has minimum 3 characters
+    public boolean isValidLastName(String lname) {
+        /**
+         *
+         * @param lastName
+         * @return boolean
+         */
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        return pattern.matcher(lname). matches();
+    }
+
 }
