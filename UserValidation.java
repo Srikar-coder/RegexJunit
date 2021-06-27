cd3a1ea37ea424ff4892c8c22b2554bb509d1ed1
Srikar <srikarsamanchi1999@gmail.com>
2021-06-27 09:18:53 +0530

    User Registration Uc3

--- UserValidation.java
+++ UserValidation.java
@@ -6,6 +6,8 @@ public class UserValidation {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
     private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
+
+    private static final String EMAIL_PATTERN = "^([a-zA-Z0-9.]+)@([a-zA-Z0-9]+).([a-zA-Z]{2,3})$";
     //First name starts with Cap and has minimum 3 characters
     public boolean isValidFirstName(String fname) {
         /**
@@ -25,5 +27,14 @@ public class UserValidation {
         Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
         return pattern.matcher(lname). matches();
     }
-
+    //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+    public boolean isValidEmailId(String emailId) {
+        /**
+         *
+         * @param emailId
+         * @return boolean
+         */
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(emailId).matches();
+    }
 }
